4e4b063072490d2c2d56bcd87fee89785c09c32b
liushi <1218522185@qq.com>
2025-05-15 09:58:21 +0800

    chaoshengpart

---
+++ DeviceChecker.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+import torch
+
+# 检查 PyTorch 是否能够识别到 GPU
+print(f"Cuda可用: {torch.cuda.is_available()}")  # 如果是 True，说明 GPU 可用
+print(f"GPU 设备编号: {torch.cuda.current_device()}")  # 查看当前 GPU 设备编号
+
+# 查看当前 GPU 的详细信息
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"GPU 型号: {torch.cuda.get_device_name(device)}")
+print(f"GPU总内存: {torch.cuda.get_device_properties(device).total_memory / 1024**3} GB")
